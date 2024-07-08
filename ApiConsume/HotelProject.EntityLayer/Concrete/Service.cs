34db37effc0adf5ba016572a6c5e4f0edcec641c
oructuran0 <orctrn193@gmail.com>
2024-07-08 14:01:54 +0300

    Creation of some entities

---
+++ ApiConsume/HotelProject.EntityLayer/Concrete/Service.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.EntityLayer.Concrete
+{
+    public class Service
+    {
+        public int ID { get; set; }
+        public string Icon { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ ApiConsume/HotelProject.EntityLayer/Concrete/Staff.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.EntityLayer.Concrete
+{
+    public class Staff
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string SocialMedia_A { get; set; }
+        public string SocialMedia_B { get; set; }
+        public string SocialMedia_C { get; set; }
+    }
+}
