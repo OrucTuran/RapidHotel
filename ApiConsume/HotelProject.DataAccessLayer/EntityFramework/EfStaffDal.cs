623f2a623f3be8774b818ee795fe42ca9e784357
oructuran0 <orctrn193@gmail.com>
2024-07-08 15:12:31 +0300

    Ef Classes

---
+++ ApiConsume/HotelProject.DataAccessLayer/EntityFramework/EfStaffDal.cs
@@ -0,0 +1,20 @@
+﻿using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.DataAccessLayer.Repositories;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.EntityFramework
+{
+    class EfStaffDal : GenericRepository<Staff>, IStaffDal
+    {
+        public EfStaffDal(Context context) : base(context)
+        {
+
+        }
+    }
+}
