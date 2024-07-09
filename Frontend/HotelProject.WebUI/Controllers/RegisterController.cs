d34507c5f06196d888233fd457a689b7e9f28d9e
oructuran0 <orctrn193@gmail.com>
2024-07-09 17:57:08 +0300

    Register DTO

---
+++ Frontend/HotelProject.WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,32 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontend/HotelProject.WebUI/DTOs/RegisterDTO/CreateNewUserDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebUI.DTOs.RegisterDTO
+{
+    public class CreateNewUserDTO
+    {
+        [Required(ErrorMessage ="Ad alanı gereklidir.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Soyad alanı gereklidir.")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Kullanıcı adı alanı gereklidir.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Mail alanı gereklidir.")]
+        public string Mail { get; set; }
+
+        [Required(ErrorMessage = "Şifre alanı gereklidir.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Şifre tekrar alanı gereklidir.")]
+        [Compare("Password",ErrorMessage ="Şifreler aynı olmalıdır.")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
