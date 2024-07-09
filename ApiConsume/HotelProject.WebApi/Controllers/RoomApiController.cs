554f5f498156463dc6ce3a4dcdd4cb1ea1e906d5
oructuran0 <orctrn193@gmail.com>
2024-07-09 15:44:45 +0300

    Get, Add and Update operations via AutoMapper

---
+++ ApiConsume/HotelProject.WebApi/Controllers/RoomApiController.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DtoLayer.DTOs.Room_DTO;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomApiController : ControllerBase
+    {
+        private readonly IRoomService _roomService;
+        private readonly IMapper _mapper;
+
+        public RoomApiController(IRoomService roomService, IMapper mapper)
+        {
+            _roomService = roomService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var values = _roomService.TGetList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult AddRoom(RoomAddDTO roomAddDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var values = _mapper.Map<Room>(roomAddDTO);
+            _roomService.TInsert(values);
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateRoom(RoomUpdateDTO roomUpdateDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var values = _mapper.Map<Room>(roomUpdateDTO);
+            _roomService.TUpdate(values);
+            return Ok();
+        }
+    }
+}
