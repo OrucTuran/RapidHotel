0cb3678f67546e95c5435598266b50184f313a15
oructuran0 <orctrn193@gmail.com>
2024-07-09 15:03:17 +0300

    AutoMapper

---
+++ ApiConsume/HotelProject.WebApi/Mapping/AutoMapperConfig.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using HotelProject.DtoLayer.DTOs.Room_DTO;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebApi.Mapping
+{
+    public class AutoMapperConfig : Profile//dto larimiz ile entitylerimiz baglayacigimiz sinif
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<RoomAddDTO, Room>();
+            CreateMap<Room, RoomAddDTO>();
+
+            CreateMap<RoomUpdateDTO, Room>().ReverseMap();
+        }
+    }
+}
+//mapleme islemi sayesinde; dto sinifinda gecmis oldugumuz properylerimizle entity sinifindaki propertylerimiz birbiri ile eslesmis olacak
