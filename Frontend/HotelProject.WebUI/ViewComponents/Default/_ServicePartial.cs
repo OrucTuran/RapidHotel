776b86e25cbc38f8992097b47495c33a61fa6303
oructuran0 <orctrn193@gmail.com>
2024-07-10 18:34:34 +0300

    the service section on the home page has been made dynamic

--- Frontend/HotelProject.WebUI/ViewComponents/Default/_ServicePartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_ServicePartial.cs
@@ -1,15 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.DTOs.ServiceDTO;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace HotelProject.WebUI.ViewComponents.Default
 {
     public class _ServicePartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ServicePartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:2424/api/Service");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDTO>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
