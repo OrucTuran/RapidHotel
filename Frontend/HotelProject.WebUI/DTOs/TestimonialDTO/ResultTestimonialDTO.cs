48b754347046afa9b4e15e6c34a02a0f3417a8c1
oructuran0 <orctrn193@gmail.com>
2024-07-10 18:48:42 +0300

    the testimonial section on the home page has been made dynamic

---
+++ Frontend/HotelProject.WebUI/DTOs/TestimonialDTO/ResultTestimonialDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebUI.DTOs.TestimonialDTO
+{
+    public class ResultTestimonialDTO
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
--- Frontend/HotelProject.WebUI/ViewComponents/Default/_TestimonialPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_TestimonialPartial.cs
@@ -1,14 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.DTOs.TestimonialDTO;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+
 namespace HotelProject.WebUI.ViewComponents.Default
 {
     public class _TestimonialPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _TestimonialPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:2424/api/Testimonial");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDTO>>(jsonData);
+                return View(values);
+            }
             return View();
-        }    }
+        }
+    }
 }
