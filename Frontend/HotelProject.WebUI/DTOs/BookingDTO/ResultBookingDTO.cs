a302957d7eb16ba70d91c1fa819c98f86a37454e
oructuran0 <orctrn193@gmail.com>
2024-07-14 20:36:23 +0300

    status buttons added to project in booking panel

---
+++ Frontend/HotelProject.WebUI/DTOs/BookingDTO/ResultBookingDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebUI.DTOs.BookingDTO
+{
+    public class ResultBookingDTO
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Mail { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        public string AdultCount { get; set; }
+        public string ChildCount { get; set; }
+        public string RoomCount { get; set; }
+        public string SpecielRequest { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+    }
+}
