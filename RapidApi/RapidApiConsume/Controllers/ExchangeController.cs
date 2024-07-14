dd51ae2d1a4bdaaaf626524eb6982be39655f5a2
oructuran0 <orctrn193@gmail.com>
2024-07-14 23:25:43 +0300

    Displaying Api (Exchange) data in a table

---
+++ RapidApi/RapidApiConsume/Controllers/ExchangeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using RapidApiConsume.Models;
+using Newtonsoft.Json;
+
+namespace RapidApiConsume.Controllers
+{
+    public class ExchangeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/metadata/exchange-rates?locale=en-gb&currency=TRY"),
+                Headers =
+    {
+        { "x-rapidapi-key", "cbf22fb3e3mshe8d554d6e847613p13e296jsn0442d6d65485" },
+        { "x-rapidapi-host", "booking-com.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ExchangeViewModel>(body);
+                return View(values.exchange_rates.ToList());
+            }
+          
+        }
+    }
+}
