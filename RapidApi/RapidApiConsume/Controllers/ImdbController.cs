a20e93d7235d403652d6e9d0ebb800ac0031a7c8
oructuran0 <orctrn193@gmail.com>
2024-07-14 22:56:02 +0300

    Displaying Api(imdb top 100 movie) data in a table

--- RapidApi/RapidApiConsume/Controllers/ImdbController.cs
+++ RapidApi/RapidApiConsume/Controllers/ImdbController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiConsume.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,27 +12,27 @@ namespace RapidApiConsume.Controllers
 {
     public class ImdbController : Controller
     {
+        List<ApiMovieViewModel> apiMovieViewModels = new List<ApiMovieViewModel>();
         public async Task<IActionResult> Index()
         {
-			
-			var client = new HttpClient();
-			var request = new HttpRequestMessage
-			{
-				Method = HttpMethod.Get,
-				RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
-				Headers =
-	{
-		{ "x-rapidapi-key", "cbf22fb3e3mshe8d554d6e847613p13e296jsn0442d6d65485" },
-		{ "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
-	},
-			};
-			using (var response = await client.SendAsync(request))
-			{
-				response.EnsureSuccessStatusCode();
-				var body = await response.Content.ReadAsStringAsync();
-			}
-
-			return View();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+                Headers =
+    {
+        { "x-rapidapi-key", "cbf22fb3e3mshe8d554d6e847613p13e296jsn0442d6d65485" },
+        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                apiMovieViewModels = JsonConvert.DeserializeObject<List<ApiMovieViewModel>>(body);
+                return View(apiMovieViewModels);
+            }
         }
     }
 }
