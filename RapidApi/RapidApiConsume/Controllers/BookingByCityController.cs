825fa938b3de45122632381ce502fd728638645c
oructuran0 <orctrn193@gmail.com>
2024-07-15 00:48:05 +0300

    list hotels by parameter with rapidApi

---
+++ RapidApi/RapidApiConsume/Controllers/BookingByCityController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiConsume.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RapidApiConsume.Controllers
+{
+    public class BookingByCityController : Controller
+    {
+        public async Task<IActionResult> Index(string cityID)
+        {
+            if (!string.IsNullOrEmpty(cityID))
+            {
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri($"https://booking-com.p.rapidapi.com/v2/hotels/search?children_number=2&locale=en-gb&children_ages=5%2C0&filter_by_currency=EUR&checkin_date=2024-09-14&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&dest_type=city&dest_id={cityID}&adults_number=2&checkout_date=2024-09-27&order_by=popularity&include_adjacency=true&room_number=1&page_number=0&units=metric"),
+                    Headers =
+    {
+        { "x-rapidapi-key", "cbf22fb3e3mshe8d554d6e847613p13e296jsn0442d6d65485" },
+        { "x-rapidapi-host", "booking-com.p.rapidapi.com" },
+    },
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    var values = JsonConvert.DeserializeObject<BookingApiViewModel>(body);
+                    return View(values.results.ToList());
+                }
+            }
+            else
+            {
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri($"https://booking-com.p.rapidapi.com/v2/hotels/search?children_number=2&locale=en-gb&children_ages=5%2C0&filter_by_currency=EUR&checkin_date=2024-09-14&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&dest_type=city&dest_id=-145692&adults_number=2&checkout_date=2024-09-27&order_by=popularity&include_adjacency=true&room_number=1&page_number=0&units=metric"),
+                    Headers =
+    {
+        { "x-rapidapi-key", "cbf22fb3e3mshe8d554d6e847613p13e296jsn0442d6d65485" },
+        { "x-rapidapi-host", "booking-com.p.rapidapi.com" },
+    },
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    var values = JsonConvert.DeserializeObject<BookingApiViewModel>(body);
+                    return View(values.results.ToList());
+                }
+            }
+
+        }
+    }
+}
