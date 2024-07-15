b8febdef3b4f3b5201c82c901b99008d65e1e5d1
oructuran0 <orctrn193@gmail.com>
2024-07-15 13:42:18 +0300

    Json Web Token creation class

--- JWTProject/WebApiJwt/Models/CreateToken.cs
+++ JWTProject/WebApiJwt/Models/CreateToken.cs
@@ -1,11 +1,34 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace WebApiJwt.Models
 {
     public class CreateToken
     {
+        public void TokenCreate()
+        {
+            var bytes = Encoding.UTF8.GetBytes("aspnetcoreapiapi");
+
+            SymmetricSecurityKey key = new SymmetricSecurityKey(bytes);
+
+            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            JwtSecurityToken token = new JwtSecurityToken
+            (
+                issuer: "http://localhost",
+                audience: "http://localhost",
+                notBefore: DateTime.Now,
+                expires: DateTime.Now.AddMinutes(3),
+                signingCredentials: credentials
+            );
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            handler.WriteToken(token);
+        }
     }
 }
