f8a1f3a3d3cbed78562f28442696bbe295ef447d
oructuran0 <orctrn193@gmail.com>
2024-08-05 19:20:56 +0300

    Message Operations

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
@@ -0,0 +1,104 @@
+﻿using HotelProject.WebUI.DTOs.ContactDTO;
+using HotelProject.WebUI.DTOs.SendMessageDTO;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminContactController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Inbox()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:2424/api/Contact");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<InboxContactDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Sendbox()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:2424/api/SendMessage");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultSendboxDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddSendMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddSendMessage(CreateSendMessageDTO createSendMessage)
+        {
+            createSendMessage.SenderMail = "admin@gmail.com";
+            createSendMessage.SenderName = "Admin";
+            createSendMessage.Date = DateTime.Today;
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createSendMessage);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:2424/api/SendMessage", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("SendBox");
+            }
+            return View();
+        }
+        public PartialViewResult SideBarAdminContactPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult SideBarAdminContactCategoryPartial()
+        {
+            return PartialView();
+        }
+        public async Task<IActionResult> MessageDetailsBySendbox(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:2424/api/SendMessage/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<GetMessageByIdDTO>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> MessageDetailsByInbox(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:2424/api/Contact/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<InboxContactDTO>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
