e6659b23163b0b26e2cce0ee0e8cab403fc9b951
oructuran0 <orctrn193@gmail.com>
2024-08-07 18:48:00 +0300

    File operations with API

---
+++ ApiConsume/HotelProject.WebApi/Controllers/FileProcessController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileProcessController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> UploadFile([FromForm] IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                var fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "Texts/" + fileName);
+
+                using var stream = new FileStream(path, FileMode.Create);
+                await file.CopyToAsync(stream);
+                stream.Close(); // Akışı kapatır
+
+                return Created("", file);
+            }
+
+            return BadRequest("File is empty.");
+        }
+    }
+}
---
+++ Frontend/HotelProject.WebUI/Controllers/AdminFileController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminFileController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                using var stream = new MemoryStream();
+                await file.CopyToAsync(stream);
+                stream.Seek(0, SeekOrigin.Begin); // Akışı başa alır
+                var bytes = stream.ToArray();
+
+                ByteArrayContent byteArrayContent = new ByteArrayContent(bytes);
+                byteArrayContent.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
+                MultipartFormDataContent multipartFormDataContent = new MultipartFormDataContent();
+                multipartFormDataContent.Add(byteArrayContent, "file", file.FileName);
+
+                using var httpClient = new HttpClient();
+                await httpClient.PostAsync("http://localhost:2424/api/FileProcess", multipartFormDataContent);
+
+                stream.Dispose(); // Akışı kapatır
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+    }
+}
