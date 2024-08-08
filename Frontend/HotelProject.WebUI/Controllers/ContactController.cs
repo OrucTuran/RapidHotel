21257e9fa5a14fed52fb4725a63b54a3e93023ca
oructuran0 <orctrn193@gmail.com>
2024-08-08 16:53:55 +0300

    Dropdown tool backend codes

--- Frontend/HotelProject.WebUI/Controllers/ContactController.cs
+++ Frontend/HotelProject.WebUI/Controllers/ContactController.cs
@@ -1,6 +1,8 @@
 ﻿using HotelProject.WebUI.DTOs.ContactDTO;
+using HotelProject.WebUI.DTOs.MessageCategoryDTO;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -20,9 +22,24 @@ namespace HotelProject.WebUI.Controllers
         {
             _httpClientFactory = httpClientFactory;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:2424/api/MessageCategory");
+
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultMessageCategoryDTO>>(jsonData);
+
+            List<SelectListItem> messageCategoryValues = (from x in values
+                                                          select new SelectListItem
+                                                          {
+                                                              Text = x.MessageCategoryName,
+                                                              Value = x.MessageCategoryID.ToString()
+                                                          }).ToList();
+            ViewBag.v = messageCategoryValues;
             return View();
+
+
         }
         [HttpGet]
         public PartialViewResult SendMessage()
---
+++ Frontend/HotelProject.WebUI/DTOs/MessageCategoryDTO/ResultMessageCategoryDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebUI.DTOs.MessageCategoryDTO
+{
+    public class ResultMessageCategoryDTO
+    {
+        public int MessageCategoryID { get; set; }
+        public string MessageCategoryName { get; set; }
+    }
+}
