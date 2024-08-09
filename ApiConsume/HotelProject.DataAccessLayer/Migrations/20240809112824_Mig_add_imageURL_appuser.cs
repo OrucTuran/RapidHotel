337c3fd9a4f2c594fd569a7357f60b3a81f220eb
oructuran0 <orctrn193@gmail.com>
2024-08-09 15:37:54 +0300

    WorkLocation table

---
+++ ApiConsume/HotelProject.DataAccessLayer/Migrations/20240809112824_Mig_add_imageURL_appuser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HotelProject.DataAccessLayer.Migrations
+{
+    public partial class Mig_add_imageURL_appuser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageURL",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageURL",
+                table: "AspNetUsers");
+        }
+    }
+}
