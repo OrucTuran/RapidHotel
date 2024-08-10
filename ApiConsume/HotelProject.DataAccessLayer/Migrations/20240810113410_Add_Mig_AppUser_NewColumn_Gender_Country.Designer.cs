8cc8365fe967d6f3688763c928fc606cd4d64a6c
oructuran0 <orctrn193@gmail.com>
2024-08-10 14:51:28 +0300

    Added "Gender" and "Country" colums to AspNetUsers table

--- ApiConsume/HotelProject.DataAccessLayer/Migrations/20240810113410_Add_Mig_AppUser_NewColumn_Gender_Country.Designer.cs
+++ ApiConsume/HotelProject.DataAccessLayer/Migrations/20240810113410_Add_Mig_AppUser_NewColumn_Gender_Country.Designer.cs
@@ -4,14 +4,16 @@ using HotelProject.DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HotelProject.DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20240810113410_Add_Mig_AppUser_NewColumn_Gender_Country")]
+    partial class Add_Mig_AppUser_NewColumn_Gender_Country
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Frontend/HotelProject.WebUI/DTOs/AppUserDTO/ResultAppUserListDTO.cs
+++ Frontend/HotelProject.WebUI/DTOs/AppUserDTO/ResultAppUserListDTO.cs
@@ -11,6 +11,8 @@ namespace HotelProject.WebUI.DTOs.AppUserDTO
         public string Surname { get; set; }
         public string City { get; set; }
         public string ImageURL { get; set; }
+        public string Country { get; set; }
+        public string Gender { get; set; }
 
     }
 }
