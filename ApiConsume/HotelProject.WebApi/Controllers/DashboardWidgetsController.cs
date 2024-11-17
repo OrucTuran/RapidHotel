53f07363c2a348138b13d7f0662534b8138a40ee
oructuran0 <orctrn193@gmail.com>
2024-11-17 21:09:07 +0300

    Comsume statistics pulled from API

---
+++ ApiConsume/HotelProject.WebApi/Controllers/DashboardWidgetsController.cs
@@ -0,0 +1,24 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardWidgetsController : ControllerBase
+    {
+        private readonly IStaffService _staffService;
+
+        public DashboardWidgetsController(IStaffService staffService)
+        {
+            _staffService = staffService;
+        }
+        [HttpGet("StaffCount")]
+        public IActionResult StaffCount()
+        {
+            var value=_staffService.TGetStaffCount();
+            return Ok(value);   
+        }
+    }
+}
